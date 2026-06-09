505a12013ff7bbcef9612bd0ff547c2578ad1655
Luigi C <lc71@llibero.it>
2026-06-09 21:55:33 +0200

    Esercizio 1/110

---
+++ esercizio001.py
@@ -0,0 +1,16 @@
+# Esercizio 1: Assegnazione di variabili
+# Crea tre variabili pr rappresentare il nome, l'età e la citta di una persona.
+# Stampa ciascuna variabile separatamente.
+# Cambia il valore di una variabile e ristampala.
+
+nome = "Alice"
+eta = 30
+citta = "Roma"
+
+print("Nome", nome)
+print("Età", eta)
+print("Città", citta)
+
+nome = "Bob"
+print("Nuovo Nome", nome)
+
