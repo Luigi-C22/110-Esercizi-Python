d5890092c5ac68842987680984a93aee6c02879a
Luigi C <lc71@llibero.it>
2026-06-10 21:59:37 +0200

    esercizio 2 -calcolatrice base

---
+++ esercizio002.py
@@ -0,0 +1,18 @@
+# Esercizio 2: Calcolatrice base
+# Chiedi all'utente di inserire due numeri
+# Utilizza gli operatori aritmetici per sommare,
+#        sottrarre, moltiplicare e dividere i numeri
+# Stampa i risultati di ciascuna operazione
+
+num1 = float(input ("Inserisci il primo numero: "))
+num2 = float(input ("Inserisci il secondo numero: "))
+somma = num1 + num2
+sottrazione = num1 - num2
+moltiplicazione = num1 * num2
+if num2 != 0:
+    divisione = num1 / num2 
+
+print("Somma:", somma)
+print("Sottrazione:", sottrazione)
+print("Moltiplicazione:", moltiplicazione)
+print("Divisione:", divisione)
