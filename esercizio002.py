5367538e6a233f39b3f4165978baf6a450095a94
Luigi C <lc71@llibero.it>
2026-06-10 22:02:06 +0200

    Esercizio 2/110

--- esercizio002.py
+++ esercizio002.py
@@ -6,13 +6,13 @@
 
 num1 = float(input ("Inserisci il primo numero: "))
 num2 = float(input ("Inserisci il secondo numero: "))
-somma = num1 + num2
+sum = num1 + num2
 sottrazione = num1 - num2
 moltiplicazione = num1 * num2
 if num2 != 0:
     divisione = num1 / num2 
 
-print("Somma:", somma)
+print("Somma:", sum)
 print("Sottrazione:", sottrazione)
 print("Moltiplicazione:", moltiplicazione)
 print("Divisione:", divisione)
