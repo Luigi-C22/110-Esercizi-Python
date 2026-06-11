3ff018b0b363df7316d70274acb34e4d87ea4402
Luigi C <lc71@llibero.it>
2026-06-11 20:37:18 +0200

    Esercizio 3/110

---
+++ esercizio003.py
@@ -0,0 +1,8 @@
+# Esercizio 3: Saluto personalizzato
+# Chiedi all'utente di inserire il proprio nome
+# Utilizza print() per creare un saluto personalizzato
+# Esempio di output: "Ciao, Alice! Benvenuto/a!"
+
+print("Ciao! Come ti chiami?")
+nome = input("Inserisci qui il tuo nome: ")
+print(f"Ciao,  {nome}! Benvenuto/a!")
