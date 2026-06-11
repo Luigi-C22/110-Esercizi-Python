231bbced59191c03138b33a0523e1caa158626ca
Luigi C <lc71@llibero.it>
2026-06-11 20:56:50 +0200

    Esercizio 4/110

---
+++ esercizio004.py
@@ -0,0 +1,11 @@
+# Esercizio 4: Formattazione di una stringa
+# Chiedi all'utente di inserire il proprio nome e cognome.
+# Concatenali e stampa il nome completo in maiuscolo.
+# utilizza il metodo .lower() per stampare il nome completo in minuscolo.
+
+print("Inserisci il tuo nome e il tuo cognome.")
+nome = input("Nome: ")
+cognome = input("Cognome: ")
+nome_completo = nome + " " + cognome
+print(f"Il tuo nome completo è: {nome_completo.upper()}")
+print(f"Il tuo nome completo è: {nome_completo.lower()}")
