80a9e42309fb94076a1003a8fc4855169f66a9c6
Luigi C <lc71@llibero.it>
2026-06-17 21:46:44 +0200

    Esercizio 10/110

---
+++ esercizio010.py
@@ -0,0 +1,12 @@
+# Esercizio 10: Controllo pari e dispari
+# Chiedi all'utente di inserire un numero positivo N.
+# Utilizza un ciclo for per stampare tutti i numeri da 1 a N,
+#            ma utilizza continue per saltare i numeri dispari.
+
+print("Inserisci un numero positivo")
+N = int(input("inserisci il numero qui: "))
+for i in range (1, N + 1):
+    if i % 2 != 0:
+        continue
+    print(i)
+    
