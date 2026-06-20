1d6415ab46272b7b173c9bf6e5d82a192356405d
Luigi C <lc71@llibero.it>
2026-06-20 22:34:50 +0200

    Esercizio 10/110

--- esercizio010.py
+++ esercizio010.py
@@ -3,10 +3,9 @@
 # Utilizza un ciclo for per stampare tutti i numeri da 1 a N,
 #            ma utilizza continue per saltare i numeri dispari.
 
-print("Inserisci un numero positivo")
+print("Immetti un numero positivo e stampo tutti i pari fino a quel numero.")
 N = int(input("inserisci il numero qui: "))
 for i in range (1, N + 1):
     if i % 2 != 0:
         continue
     print(i)
-    
