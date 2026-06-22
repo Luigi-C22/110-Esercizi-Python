fcf1cf0e17d0d1f54951934ef11fb5724d69e8e9
Luigi C <lc71@llibero.it>
2026-06-22 21:50:03 +0200

    Esercizio 12/110

---
+++ esercizio012.py
@@ -0,0 +1,8 @@
+# Esercizio 12: Definisci una funzione chimata benvenuto() 
+#               che stampa il messaggio "Benvenuto al corso di Python!".
+# chiama la funzione per visualizzare il messaggio.
+
+def benvenuto():
+    print("Benvenuto al corso di Python!")
+
+benvenuto()
