66fb814aef84221bada52a4569e3523fe9036cdb
Luigi C <lc71@llibero.it>
2026-06-24 21:35:52 +0200

    Esercizio 15/110

---
+++ esercizio015.py
@@ -0,0 +1,13 @@
+# Esercizio 15: Differenza tra Scope Locale e Globale
+# Crea una variabile numero globale.
+# Crea una funzione modifica_numero che modifica numero all'interno della funzione.
+# Stampa il valore di numero prima e dopo la chiamata alla funzione per vedere come cambia.
+
+numero = int(input("Immetti unnumero: "))  # Variabile globale
+def modifica_numero():
+    global numero
+    numero = numero *2  # Modifica la variabile globale
+
+print("Prima della chiamata:", numero)
+modifica_numero()
+print("Dopo la chiamata:", numero)
