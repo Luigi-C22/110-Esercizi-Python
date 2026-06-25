022b483bc62ced952227c8cb1ffa05ad03f12cb5
Luigi C <lc71@llibero.it>
2026-06-25 21:20:38 +0200

    Esercizio 15/110

--- esercizio015.py
+++ esercizio015.py
@@ -3,7 +3,7 @@
 # Crea una funzione modifica_numero che modifica numero all'interno della funzione.
 # Stampa il valore di numero prima e dopo la chiamata alla funzione per vedere come cambia.
 
-numero = int(input("Immetti unnumero: "))  # Variabile globale
+numero = int(input("Immetti un numero: "))  # Variabile globale
 def modifica_numero():
     global numero
     numero = numero *2  # Modifica la variabile globale
