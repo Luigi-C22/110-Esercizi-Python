7b1ff399df0fb83fc225b3390c59406c3e0b92cf
Luigi C <lc71@llibero.it>
2026-06-28 21:57:13 +0200

    Esercizio 19/110

---
+++ esercizio019.py
@@ -0,0 +1,8 @@
+# Esercizio 19: Crea una tupla con tre colori.
+# Stampa il secondo colore della tupla.
+# Tenta di aggiungere un nuovo colore e nota l'errore per capire l'immutabilità delle tuple.
+
+colori = ("bianco", "rosso", "verde")
+print(colori[1])
+
+colori.append("giallo")
