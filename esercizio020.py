8be21bd49f16454cdafb53dfd01aabfa9e31fab6
Luigi C <lc71@llibero.it>
2026-06-29 21:39:03 +0200

    Esercizio 20/110

---
+++ esercizio020.py
@@ -0,0 +1,17 @@
+# Esercizio 20: Creazione e Manipolazione di un Dizionario
+# Crea un dizionario per un libro con le seguenti chiavi: titolo, autore e anno
+# Aggiungi una nuova chiave genere al dizionario.
+# Aggiorna l'anno di pubblicazione.
+# Stampa il dizionario aggiornato
+
+titolo = input("inserisci titolo: ")
+autore = input("inserisci autore: ")
+anno = int(input("Inserisci l'anno: "))
+
+
+libro = {"Titolo":titolo, 
+         "Autore":autore,
+         "Anno":anno}
+libro["Genere"] = "Romanzo"
+libro["Anno"] = 1915
+print(libro)
