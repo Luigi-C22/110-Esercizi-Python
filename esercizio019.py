9a4462a86391b2ef4300e152d2208720608bba72
Luigi C <lc71@llibero.it>
2026-06-29 21:14:32 +0200

    Esercizio 19/110

--- esercizio019.py
+++ esercizio019.py
@@ -2,14 +2,7 @@
 # Stampa il secondo colore della tupla.
 # Tenta di aggiungere un nuovo colore e nota l'errore per capire l'immutabilità delle tuple.
 
-# L'utente inserisce ad esempio: cane, gatto, criceto
-risposta = input("Inserisci gli animali separati da una virgola: ")
+colori = ("bianco", "rosso", "verde")
+print(colori[1])
 
-# split(",") crea una lista separando le parole dove c'è la virgola, 
-# e tuple() la trasforma immediatamente
-#animali_tupla = tuple(risposta.split(","))
-
-# Puliamo eventuali spazi bianchi di troppo attorno alle parole (opzionale)
-animali_tupla = tuple(animale.strip() for animale in animali_tupla)
-
-print("La tua tupla:", animali_tupla)
+colori.append("giallo")
