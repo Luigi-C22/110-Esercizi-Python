58986742978587d871c47546a36c46b94712b598
Luigi C <lc71@llibero.it>
2026-07-02 21:50:44 +0200

    Esercizio 23/110

---
+++ esercizio023.py
@@ -0,0 +1,7 @@
+# Esercizio 23: Sistema di Inventario per un Negozio
+# Crea un dizionario per rappresentare l'inventario del negozio, con i prodotti come
+#      chiavi e i loro prezzi come valori.
+# Usa una lista per registrare le vendite di vari prodotti.
+# Aggiorna l'inventario sottraendo gli articoli venduti.
+# Stampa il valore totale delle vendite e l'inventario aggiornato.
+
