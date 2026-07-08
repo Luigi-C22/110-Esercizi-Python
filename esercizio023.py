c7201956094b347336ddc3d49ffaf9e415b3cad3
Luigi C <lc71@llibero.it>
2026-07-08 21:36:56 +0200

    Esercizio 23/110

--- esercizio023.py
+++ esercizio023.py
@@ -5,3 +5,14 @@
 # Aggiorna l'inventario sottraendo gli articoli venduti.
 # Stampa il valore totale delle vendite e l'inventario aggiornato.
 
+inventario = {
+    "mela": 0.5,
+    "banana": 0.3,
+    "arancia": 0.4,
+    "pera": 0.6,
+    "pesca": 0.7,
+    "kiwi": 0.8
+}
+
+vendite = ["mela", "banana", "arancia", "mela", "kiwi", "pera", "banana"]
+    
