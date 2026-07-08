8ff97bef6f46b30a5d526d5fedbb61e6000fbca9
Luigi C <lc71@llibero.it>
2026-07-08 21:48:28 +0200

    Esercizio 23/110

--- esercizio023.py
+++ esercizio023.py
@@ -6,13 +6,12 @@
 # Stampa il valore totale delle vendite e l'inventario aggiornato.
 
 inventario = {
-    "mela": 0.5,
-    "banana": 0.3,
-    "arancia": 0.4,
-    "pera": 0.6,
-    "pesca": 0.7,
-    "kiwi": 0.8
+    "mela": {"prezzo":0.5, "quantità": 10},
+    "banana": {"prezzo":0.3, "quantità": 15},
+    "arancia": {"prezzo":0.4, "quantità": 8},
+    "pera": {"prezzo":0.6, "quantità": 12},
+    "pesca": {"prezzo":0.7, "quantità": 5},
+    "kiwi": {"prezzo":0.8, "quantità": 7}
 }
 
 vendite = ["mela", "banana", "arancia", "mela", "kiwi", "pera", "banana"]
-    
