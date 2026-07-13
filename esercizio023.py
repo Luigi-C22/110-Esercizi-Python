17fdb8d56755dff9ebd273a9c1ac103481766a15
Luigi C <lc71@llibero.it>
2026-07-13 22:13:16 +0200

    Eserizio 23/110

--- esercizio023.py
+++ esercizio023.py
@@ -1,24 +1,22 @@
 # Esercizio 23: Sistema di Inventario per un Negozio
+
 # Crea un dizionario per rappresentare l'inventario del negozio, con i prodotti come
 #      chiavi e i loro prezzi come valori.
-
-# Usa una lista per registrare le vendite di vari prodotti.
-
-# Aggiorna l'inventario sottraendo gli articoli venduti.
-
-# Stampa il valore totale delle vendite e l'inventario aggiornato.
-
 inventario = {
     "mela": {"prezzo":0.5, "quantità": 10},
-    "banana": {"prezzo":0.3, "quantità": 15},
+    "banana": {"prezzo":1, "quantità": 15},
     "arancia": {"prezzo":0.4, "quantità": 8},
     "pera": {"prezzo":0.6, "quantità": 12},
     "pesca": {"prezzo":0.7, "quantità": 5},
-    "kiwi": {"prezzo":0.8, "quantità": 7}
+    "kiwi": {"prezzo":0.8, "quantità": 7},
+    "uva": {"prezzo":1.0, "quantità": 20},
+    "mango": {"prezzo":1.5, "quantità": 3},
 }
 
-vendite = ["mela", "banana", "arancia", "mela", "kiwi", "pera", "banana", "mela"]
+# Usa una lista per registrare le vendite di vari prodotti.
+vendite = ["mela", "banana", "arancia","mango", "mango", "mela", "kiwi", "pera", "banana", "mela"]
 
+# Aggiorna l'inventario sottraendo gli articoli venduti.
 totale_vendite = 0
 for prodotto in vendite:
         if prodotto in inventario and inventario[prodotto]["quantità"] > 0:
@@ -27,6 +25,7 @@ for prodotto in vendite:
         else:
             print(f"Prodotto {prodotto} non disponibile o esaurito.")
 
+# Stampa il valore totale delle vendite e l'inventario aggiornato.
 print(f"Valore totale delle vendite: {totale_vendite}")
 
 print("Inventario aggiornato:")
