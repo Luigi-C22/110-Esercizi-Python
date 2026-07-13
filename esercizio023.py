00ae711b52d9ba4be79cc1e453c94407db63d9c5
Luigi C <lc71@llibero.it>
2026-07-13 21:55:15 +0200

    Esercizio 23/110

--- esercizio023.py
+++ esercizio023.py
@@ -1,8 +1,11 @@
 # Esercizio 23: Sistema di Inventario per un Negozio
 # Crea un dizionario per rappresentare l'inventario del negozio, con i prodotti come
 #      chiavi e i loro prezzi come valori.
+
 # Usa una lista per registrare le vendite di vari prodotti.
+
 # Aggiorna l'inventario sottraendo gli articoli venduti.
+
 # Stampa il valore totale delle vendite e l'inventario aggiornato.
 
 inventario = {
@@ -14,4 +17,18 @@ inventario = {
     "kiwi": {"prezzo":0.8, "quantità": 7}
 }
 
-vendite = ["mela", "banana", "arancia", "mela", "kiwi", "pera", "banana"]
+vendite = ["mela", "banana", "arancia", "mela", "kiwi", "pera", "banana", "mela"]
+
+totale_vendite = 0
+for prodotto in vendite:
+        if prodotto in inventario and inventario[prodotto]["quantità"] > 0:
+            totale_vendite += inventario[prodotto]["prezzo"]
+            inventario[prodotto]["quantità"] -= 1
+        else:
+            print(f"Prodotto {prodotto} non disponibile o esaurito.")
+
+print(f"Valore totale delle vendite: {totale_vendite}")
+
+print("Inventario aggiornato:")
+for prodotto, dettagli in inventario.items():
+    print(f"{prodotto}: {dettagli['quantità']} unità disponibili")
