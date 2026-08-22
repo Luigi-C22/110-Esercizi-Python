b03d01c5a408bc2b79475082ac409093e28adba7
Luigi C <lc71@llibero.it>
2026-08-22 21:47:56 +0200

    Esercizio 31/110

--- esercizio031.py
+++ esercizio031.py
@@ -1,4 +1,8 @@
-#Esempio
+# Esercizio 31: Creazione attributi e metodi
+# Crea una classe Libro con attributi titolo e autore.
+# Aggiungi un metodo descrizione che stampa le informazioni del libro.
+# Crea una istanza della classe libro e chiama il metodo descrizione.
+
 class Libro:
     def __init__(self, titolo, autore):
         self.titolo = titolo
@@ -10,5 +14,4 @@ class Libro:
 descrizione_libro = Libro("Il Signore degli Anelli", "J.R.R. Tolkien")
 descrizione_libro.descrizione()
 
-# Esercizio 31: Creazione attributi e metodi
 
