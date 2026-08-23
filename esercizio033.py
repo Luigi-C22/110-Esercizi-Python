dd7e4ab592900a40ef9e98bc1c50328a6939fddd
Luigi C <lc71@llibero.it>
2026-08-23 21:36:13 +0200

    Esercizio 33/110

--- esercizio033.py
+++ esercizio033.py
@@ -1,6 +1,6 @@
 #Esempio
-class Animale:
-    def __init(self, nome):
+""" class Animale:
+    def __init__(self, nome):
         self.nome = nome
 
     def parla(self):
@@ -12,7 +12,7 @@ class Cane(Animale):
 
 class Gatto(Animale):
     def parla(self):
-        print("Meow!")
+        print("Meow!") """
 
 
 
@@ -21,3 +21,18 @@ class Gatto(Animale):
 # Crea una sottoclasse Auto che eredita da Veicolo e aggiunge un metodo tipo che stampa "Questa è un'auto."
 # Crea una istanza della classe Auto e chiama i metodi descrizione e tipo.
 
+class Veicolo:
+    def __init__(self, marca):
+        self.marca = marca
+
+    def descrizione(self):
+        print("Marca del veicolo:", self.marca)
+
+class Auto(Veicolo):
+        def tipo(self):
+             print("Questa è un'auto")
+
+
+auto = Auto("Mercedes")
+auto.descrizione()
+auto.tipo()
