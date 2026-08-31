277de32a368d0470aad6fe04df0f2fb212e4f2e3
Luigi C <lc71@llibero.it>
2026-08-31 21:38:32 +0200

    Esercizio 34/110

--- esercizio034.py
+++ esercizio034.py
@@ -18,8 +18,26 @@ class Cerchio(Forma):
     def area(self):
         return 3.14 * (self.raggio **2) """
 
-# Esercizio 34: Poliorfismo con classi.
+# Esercizio 34: Polimorfismo con classi.
 #Crea una classe Animale con un metodo suono.
 # Crea le sottoclassi Cane e Gatto che ridefiniscono il metodo suono 
 #                     per stampare rispettivamente "Woof" e "Meow".
 # Crea una istanza di entrambe le classi e chiama il metodo suono.
+
+class Animale:
+    def suono(self):
+        pass
+
+class Cane(Animale):
+    def suono(self):
+        return "Woof"   
+
+class Gatto(Animale):
+    def suono(self):
+        return "Meow"
+
+cane = Cane()
+gatto = Gatto()
+print("il verso del cane è: " + cane.suono())
+print("il verso del gatto è: " + gatto.suono())
+
