9ca44b84162e1f764611f3a86f70082dea8fde97
Luigi C <lc71@llibero.it>
2026-09-03 22:05:18 +0200

    Esercizio 36/110

--- esercizio036.py
+++ esercizio036.py
@@ -1,6 +1,13 @@
 #Esempio di lettura file
-with open("file_di_testo.txt", "r") as file:
+""" with open("file_di_testo.txt", "r") as file:
     contenuto = file.read()
     print(contenuto)
+ """
+# Esercizio 36: Lettura di un File di Testo
+# Crea un file di testo saluti.txt contenente alcuni saluti in diverse lingue.
+# Scrivi un programma che legge il contenuto del file e lo stampa a schermo.
 
+with open("saluti.txt", "r") as file:
+    contenuto_del_file = file.read()
+    print(contenuto_del_file)
     
