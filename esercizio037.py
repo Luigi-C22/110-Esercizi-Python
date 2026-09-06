df8b0f1d74da7af54007ca3cfadfdafd5a3fa4a8
Luigi Ciangola <lc71@libero.it>
2026-09-06 18:39:25 +0200

    Esercizio 37/110

--- esercizio037.py
+++ esercizio037.py
@@ -1,8 +1,8 @@
 #Esempio di scrittura in un file:
-with open("output.txt", "w") as file:
+""" with open("output.txt", "w") as file:
     file.write("Ciao mondo!\n")
     file.write("Questo é un file di testo.\n")
-    file.write("Saluti a tutti!")
+    file.write("Saluti a tutti!") """
 
 # Esercizio 37: Scrittura su un File di Testo
 # Scrivi un programma che chiede all'utente di inserire il proprio nome.
@@ -13,3 +13,4 @@ print("Devi inserire il tuo nome per salvarlo in un file di testo")
 nome = input("Inserisci qui il tuo nome: ")
 with open("nomi.txt", "a") as file:
     file.write(nome + "\n")
+
