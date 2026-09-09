d4581d4cf665dfd037520a3f4ef2c5acc4a59640
Luigi C <lc71@llibero.it>
2026-09-09 21:56:44 +0200

    Esercizio 40/110

--- esercizio040.py
+++ esercizio040.py
@@ -5,4 +5,8 @@ with open("dati.csv", "r") as file:
     lettore_csv = csv.reader(file)
     for riga in lettore_csv:
         print(riga)
-        
+
+
+# Esercizio 40: Lettura di file CSV
+# Crea un file studenti.csv contenente i nomi e le età di alcuni studenti.
+# Scrivi un programma che legge il file e stampa i dati di ogni singolo studente.
