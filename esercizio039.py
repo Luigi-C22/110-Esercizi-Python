5108fc2220c2263fec20248e24312ed25d71db64
Luigi C <lc71@llibero.it>
2026-09-09 21:30:11 +0200

    Esercizio 39/110

--- esercizio039.py
+++ esercizio039.py
@@ -6,4 +6,15 @@ try:
         contenuto = file.read()
 except FileNotFoundError:
     print("Errore: il file non esiste")
+
+
+# esercizio 39: Gestione errori nella lettura e scrittura files
+# Scrivi un programma che tenta di aprire un file dati.txt.
+# Se il programma non esiste, deve stampare "il file non è stato trovato".
+
+try: 
+    with open("dati.txt", "r") as file:
+        contenuto_del_file = file.read()
+except FileNotFoundError:
+    print("il file non è stato trovato")
     
