987ca4d5dc81e1a09916429e4f5a2d2c5b438b54
Luigi C <lc71@llibero.it>
2026-09-15 18:13:47 +0200

    Esercizio 41/110

--- esercizio041.py
+++ esercizio041.py
@@ -6,3 +6,8 @@ dati = [["Nome", "Età"], ["Alice", 22], ["Bob", 25]]
 with open("output.csv", "w", newline = "") as file:
     scrittore_csv = csv.writer(file)
     scrittore_csv.writerows(dati)
+
+with open("output.csv", "r") as file:
+    lettore_csv = csv.reader(file)
+    for riga in lettore_csv:
+        print(riga)
