1f5214e604f8eab4e27ab3c995fedbe1fef6f5e9
Luigi C <lc71@llibero.it>
2026-09-26 23:17:44 +0200

    Esercizio 43/110

---
+++ esercizio043.py
@@ -0,0 +1,7 @@
+# Esempio della libreria Math
+import math
+raggio = 5 
+area_cerchio = math.pi * raggio ** 2
+circonferenza = 2 * math.pi * raggio
+print(f"L'area del cerchio con raggio {raggio} è: {area_cerchio:.3f}")
+print(f"La circonferenza del cerchio con raggio {raggio} è: {circonferenza:.3f}")
